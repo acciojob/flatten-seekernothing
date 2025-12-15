f834356f418bc8f594ae1f261f93837d3c900cf2
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-12-16 03:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //your JS code here. If required.
 function flatten(value) {
+	let res = []
+	for(let i = 0 ;i<value.length;i++){
+		if(Array.isArray(value[i])){
+			let flat = flatten(arr[i])
+			res.push(...flat)
+		}else{
+			res.push(value[i])
+		}
+	}
+
+	return res
  
 }
 
