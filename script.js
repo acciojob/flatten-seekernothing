3f3254508730a0341bc5d91e7688ea440156368b
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-12-16 04:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function flatten(value) {
 	let res = []
 	for(let i = 0 ;i<value.length;i++){
 		if(Array.isArray(value[i])){
-			let flat = flatten(arr[i])
+			let flat = flatten(value[i])
 			res.push(...flat)
 		}else{
 			res.push(value[i])
